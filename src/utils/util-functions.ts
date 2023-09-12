89f07b6510e41c668b613e157bcdedf4d4a4426f
theobourgeois <theoobourgeois@gmail.com>
2023-09-11 21:11:29 -0300

    rm unused code

--- src/utils/util-functions.ts
+++ src/utils/util-functions.ts
@@ -1,5 +1,4 @@
-import { EQUALIZER_DIMENSIONS } from "./constants";
-import { idGen } from "./globals";
+import {idGen} from "./globals";
 
 export function* idGenerator() {
     let id = 0;
@@ -7,9 +6,7 @@ export function* idGenerator() {
         yield ++id;
     }
 }
+
 export const getNewID = () => idGen.next().value as number;
 
-export const frequencyToX = (frequency: number) => {
-    return (frequency / 20000) * EQUALIZER_DIMENSIONS.width;
-}
 
